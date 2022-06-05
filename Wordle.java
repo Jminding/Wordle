5bb95bc17e33d3eb1142f9801981652e1af1bbbd
Jaymin Ding <jtding43@gmail.com>
2022-06-05 16:50:49 -0400

    Should correctly color all words now

--- Wordle.java
+++ Wordle.java
@@ -6,14 +6,14 @@
 
 import edu.willamette.cs1.wordle.WordleDictionary;
 import edu.willamette.cs1.wordle.WordleGWindow;
+
+import java.awt.*;
 import java.util.*;
 
 public class Wordle {
 
     final String correctWord = WordleDictionary.FIVE_LETTER_WORDS[(int) (Math.random() * WordleDictionary.FIVE_LETTER_WORDS.length)].toUpperCase();
-    private HashMap<String, Integer> guessCharCount = new HashMap<>(); // the number of each character in the guess
     private HashMap<String, Integer> answerCharCount = new HashMap<>(); // the number of each character in the answer
-    private HashMap<String, Integer> correctCharCount = new HashMap<>(); // the number of correct characters in the guess
     private HashMap<String, Integer> currChar = new HashMap<>(); // the number of the current character
 
     public void run() {
@@ -39,20 +39,9 @@ public class Wordle {
          * else if it is not make the user try again and don't color the squares
          */
         int pos = Collections.binarySearch(Arrays.asList(WordleDictionary.FIVE_LETTER_WORDS), s.toLowerCase());
-        guessCharCount.clear();
         currChar.clear();
-        correctCharCount.clear();
         if (pos >= 0) {
             numTries++;
-            for (int i = 0; i < 5; i++) {
-                if (!guessCharCount.containsKey(s.substring(i, i + 1))) guessCharCount.put(s.substring(i, i + 1), 1);
-                else guessCharCount.put(s.substring(i, i + 1), guessCharCount.get(s.substring(i, i + 1)) + 1);
-                if (s.charAt(i) == correctWord.charAt(i)) {
-                    if (!correctCharCount.containsKey(s.substring(i, i + 1))) correctCharCount.put(s.substring(i, i + 1), 1);
-                    else correctCharCount.put(s.substring(i, i + 1), correctCharCount.get(s.substring(i, i + 1)) + 1);
-                }
-            }
-            // gw.showMessage("Valid word!");
             if (!s.equals(correctWord)) {
                 if (numTries == 6) {
                     colorSquares(s);
@@ -79,32 +68,35 @@ public class Wordle {
 
                 }
             }
-            
+        } else if (s.length() < 5) {
+            gw.showMessage("Not enough letters");
         } else {
             gw.showMessage("Not in word list");
         }
     }
 
     public void colorSquares(String s) {
-        for (int i = 0; i < 5; i++) {
-            if (!currChar.containsKey(s.substring(i, i + 1))) currChar.put(s.substring(i, i + 1), 1);
-            else currChar.put(s.substring(i, i + 1), currChar.get(s.substring(i, i + 1)) + 1);
+        for (int i = 0; i < 5; i++) { // Color in all the ones that are correct first
             if (s.charAt(i) == correctWord.charAt(i)) {
+                if (!currChar.containsKey(s.substring(i, i + 1))) currChar.put(s.substring(i, i + 1), 1);
+                else currChar.put(s.substring(i, i + 1), currChar.get(s.substring(i, i + 1)) + 1);
                 gw.setSquareColor(gw.getCurrentRow(), i, gw.CORRECT_COLOR);
                 gw.setKeyColor(s.substring(i, i + 1), gw.CORRECT_COLOR);
-            } else if (correctWord.contains(s.substring(i, i + 1)) && correctWord.charAt(i) != s.charAt(i)) {
-                if (correctCharCount.containsKey(s.substring(i, i + 1)) && currChar.get(s.substring(i, i + 1)) + correctCharCount.get(s.substring(i, i + 1)) <= answerCharCount.get(s.substring(i, i + 1))) {
+            }
+        }
+        for (int i = 0; i < 5; i++) { // Then color in the ones that are not in the correct position
+            if (correctWord.contains(s.substring(i, i + 1)) && correctWord.charAt(i) != s.charAt(i) && !gw.getSquareColor(gw.getCurrentRow(), i).equals(gw.CORRECT_COLOR)) {
+                if (!currChar.containsKey(s.substring(i, i + 1))) currChar.put(s.substring(i, i + 1), 1);
+                else currChar.put(s.substring(i, i + 1), currChar.get(s.substring(i, i + 1)) + 1);
+                if (currChar.get(s.substring(i, i + 1)) <= answerCharCount.get(s.substring(i, i + 1))) {
                     gw.setSquareColor(gw.getCurrentRow(), i, gw.PRESENT_COLOR);
                     if (!gw.getKeyColor(s.substring(i, i + 1)).equals(gw.CORRECT_COLOR))
                         gw.setKeyColor(s.substring(i, i + 1), gw.PRESENT_COLOR);
-                } else if (guessCharCount.get(s.substring(i, i + 1)) <= answerCharCount.get(s.substring(i, i + 1))) {
-                    if (!gw.getKeyColor(s.substring(i, i + 1)).equals(gw.CORRECT_COLOR))
-                        gw.setKeyColor(s.substring(i, i + 1), gw.PRESENT_COLOR);
-                    gw.setSquareColor(gw.getCurrentRow(), i, gw.PRESENT_COLOR);
-                } else {
-                    gw.setSquareColor(gw.getCurrentRow(), i, gw.MISSING_COLOR);
                 }
-            } else {
+            }
+        }
+        for (int i = 0; i < 5; i++) { // Then color in any remaining ones as gray
+            if (gw.getSquareColor(gw.getCurrentRow(), i).equals(Color.WHITE)) { // If it isn't colored in already then it's incorrect
                 gw.setSquareColor(gw.getCurrentRow(), i, gw.MISSING_COLOR);
                 gw.setKeyColor(s.substring(i, i + 1), gw.MISSING_COLOR);
             }
@@ -121,5 +113,5 @@ public class Wordle {
 /* Private instance variables */
 
     private WordleGWindow gw;
-    protected static int numTries = 0;
+    private int numTries = 0;
 }
